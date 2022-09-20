ded27c0eb7ae645f2f7b2a53050f83fd714ec8a3
Pedro Henrique <qap22bpm@gmail.com>
2022-09-20 10:58:52 -0300

    Add files via upload

---
+++ assets/script/index.js
@@ -0,0 +1,11 @@
+function add(){
+    var selectValue = document.getElementById("select")
+    var nameValue = document.getElementById("name")
+    var linkValue = document.getElementById("link")
+    var imageValue = document.getElementById("image")
+
+    const taskItemContainer = document.createElement("div");
+    taskItemContainer.classList.add("task-item");
+
+
+}
